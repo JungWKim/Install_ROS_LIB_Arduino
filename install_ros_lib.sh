a1e36af79741e26015dbcc4f6ffbb4f97b62718a
JWooKim <lionmini@naver.com>
2020-08-06 02:36:40 +0900

    finished install_ros_lib.sh

--- install_ros_lib.sh
+++ install_ros_lib.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-
-sudo apt install ros-melodic
+cd
+sudo apt-get install ros-melodic-rosserial-arduino
+sudo apt-get install ros-melodic-rosserial
+sudo apt-get install ros-melodic-rosserial-server
+cd ~/arduino-1.8.13/libraries
+sudo rm -rf ros_lib
+rosrun rosserial_arduino make_libraries.py .
